0af3fedaea19615fb04ea876e62fda809076f34a
Sergio Burgueno <burguer@gmail.com>
2013-09-24 21:53:07 -0700

    01- Inicio
    rails new fotoblog

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fotoblog::Application.config.secret_token = '8a46242bf714b18404e47c29693d51898a0c1049295b6c46b36133c192e8d7d07c0cc18443181a6b74c3e6e8abbb44e5c67f8285f2606f59088694bcc870c80c'
