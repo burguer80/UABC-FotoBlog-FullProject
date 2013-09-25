b871c6f290bce1696603b271b5544f1ebb48b21f
Sergio Burgueno <burguer@gmail.com>
2013-09-25 01:24:20 -0700

    06 Plantillas, flash
    + Layouts
    + flash[:notice]

--- app/controllers/blog_controller.rb
+++ app/controllers/blog_controller.rb
@@ -11,8 +11,10 @@ class BlogController < ApplicationController
   def guardar_foto
   	@foto = Foto.new(params[:foto])
   	if @foto.save
+  		flash[:notice] = "Se ha cargado correctamente tu foto"
     	redirect_to :action => 'index'
 	else
+		flash[:notice] = "Titulo y foto son obligatorios"
 		render :action => 'foto_nueva'
 	end
   end	
