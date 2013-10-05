759aae20e63e132c23fa68df37179c8b5071f678
Sergio Burgueno <burguer@gmail.com>
2013-10-05 01:27:48 -0700

    instalacion y conf para pdf con fondo

--- Gemfile
+++ Gemfile
@@ -7,6 +7,7 @@ gem 'rails', '3.2.11'
 
 gem 'sqlite3'
 gem 'carrierwave'
+gem 'prawn'
 
 
 # Gems used only for assets and not required
--- app/models/foto.rb
+++ app/models/foto.rb
@@ -2,4 +2,17 @@ class Foto < ActiveRecord::Base
   attr_accessible :descripcion, :titulo, :imagen
   mount_uploader :imagen, ImagenUploader
   validates_presence_of :titulo, :imagen
+
+
+
+def self.pdf
+	Prawn::Document.generate("pdf/implicit.pdf", :page_layout => :landscape) do
+    	image "#{Rails.root}/constancias/constancia.jpg", :at => [-35, 577]
+    	text 	  "Hello World"
+		#start_new_page
+		text "See. We've left the previous page behind.", :color => "FF9900"
+		text_box "F.", :kerning => true,  :at => [500, y - 10]
+
+	end	
+end
 end
